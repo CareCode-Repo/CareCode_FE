889631a02f9f7e0cabddafd3267d1df981cea762
leejimin <jimin980915@naver.com>
2025-08-19 02:36:47 +0900

    feat: 커뮤니티 메인페이지 게시글 리스크 쿼리 연결 및 커스텀 훅 구현

--- src/queries/community.ts
+++ src/queries/community.ts
@@ -1,20 +1,35 @@
 import { createQueryKeys } from '@lukemorales/query-key-factory'
-import { getCommunityPostById, getCommunityPosts } from '@/apis/community'
-import { GetCommunityPostByIdPath, GetCommunityPostsQuery } from '@/types/apis/community'
+import { InfiniteData, useInfiniteQuery, UseInfiniteQueryResult } from '@tanstack/react-query'
+import { getCommunityPosts } from '@/apis/community'
+import {
+  GetCommunityPostByIdPath,
+  GetCommunityPostsQuery,
+  GetCommunityPostsResponse,
+} from '@/types/apis/community'
 
 export const communityQueries = createQueryKeys('community', {
   list: (query: GetCommunityPostsQuery) => ({
     queryKey: ['list', query],
-    queryFn: () => getCommunityPosts(query),
   }),
 
   detail: (postId: GetCommunityPostByIdPath['postId']) => ({
     queryKey: ['detail', { postId }],
-    queryFn: () => getCommunityPostById({ postId }),
   }),
 
   comments: (postId: GetCommunityPostByIdPath['postId']) => ({
     queryKey: ['comments', { postId }],
-    queryFn: () => getCommunityPostById({ postId }).then((res) => res.comments),
   }),
 })
+
+export const useGetCommunityPosts = (
+  query: GetCommunityPostsQuery,
+): UseInfiniteQueryResult<InfiniteData<GetCommunityPostsResponse>, Error> => {
+  return useInfiniteQuery({
+    queryKey: communityQueries.list(query).queryKey,
+    initialPageParam: 1,
+    queryFn: async ({ pageParam = 1 }: { pageParam: number }) =>
+      await getCommunityPosts({ ...query, page: pageParam }),
+    getNextPageParam: (lastPage: GetCommunityPostsResponse) =>
+      lastPage.page < lastPage.totalPages ? lastPage.page + 1 : undefined,
+  })
+}
