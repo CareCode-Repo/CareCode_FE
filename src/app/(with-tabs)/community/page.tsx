357b8026751cc59adcf2588decee48a51b60022c
leejimin <jimin980915@naver.com>
2025-08-20 13:55:46 +0900

    feat: 게시글 목록 API 연결

--- src/app/(with-tabs)/community/page.tsx
+++ src/app/(with-tabs)/community/page.tsx
@@ -1,63 +1,49 @@
 'use client'
 
-import { JSX, useCallback, useEffect, useRef, useState } from 'react'
-import { dummyCommunityPosts } from './postListDummy'
+import { JSX, useEffect, useRef } from 'react'
+import { usePosts } from './hooks/usePosts'
+import BabyIcon from '@/assets/icons/baby.svg'
 import BellIcon from '@/assets/icons/bell.svg'
 import SearchIcon from '@/assets/icons/search.svg'
 import Input from '@/components/common/input'
 import { useInput } from '@/components/common/input/hooks/useInput'
 import TopNavBar from '@/components/common/top-navbar'
 import IconButton from '@/components/common/top-navbar/IconButton'
-import CommunityPost from '@/components/features/community/CommunityPost'
-
 import NewPostFAB from '@/components/features/community/NewPostFAB'
-import { PostListItem } from '@/types/apis/community'
-
-const POSTS_PER_PAGE = 3
+import CommunityPost from '@/components/features/community/community-post-list'
 
 const Community = (): JSX.Element => {
-  const [page, setPage] = useState<number>(1)
-  const [posts, setPosts] = useState<PostListItem[]>(dummyCommunityPosts.slice(0, 3))
-  const loader = useRef<HTMLDivElement>(null)
+  const { posts, fetchNextPage, hasNextPage, isFetchingNextPage } = usePosts({ page: 0, size: 10 })
   const searchInput = useInput('')
+  const loader = useRef<HTMLDivElement>(null)
+
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   const handleSearch = () => {}
 
-  const loadMore = useCallback(() => {
-    const nextPage = page + 1
-    const start = page * POSTS_PER_PAGE
-    const end = start + POSTS_PER_PAGE
-    const nextPosts = dummyCommunityPosts.slice(start, end)
-
-    if (nextPosts.length > 0) {
-      setPosts((prevPosts) => [...prevPosts, ...nextPosts])
-      setPage(nextPage)
-    }
-  }, [page]) // page가 변경될 때마다 이 함수가 재생성됩니다.
-
   useEffect(() => {
     const currentLoader = loader.current
+    if (!currentLoader) return
+
     const observer = new IntersectionObserver(
       (entries) => {
-        // isIntersecting이 true일 때만 loadMore를 호출하도록 확인
-        if (entries[0].isIntersecting) {
-          loadMore()
+        const entry = entries[0]
+        if (entry.isIntersecting && !isFetchingNextPage) {
+          fetchNextPage()
         }
       },
-      { threshold: 0.8 },
+      {
+        root: null, // viewport 기준
+        rootMargin: '200px', // 화면 아래 200px에 들어오면 호출
+        threshold: 0, // entry가 조금이라도 보이면 실행
+      },
     )
 
-    if (currentLoader) {
-      observer.observe(currentLoader)
-    }
+    observer.observe(currentLoader)
 
-    // cleanup 함수에서 observer 연결을 해제합니다.
     return () => {
-      if (currentLoader) {
-        observer.unobserve(currentLoader)
-      }
+      observer.unobserve(currentLoader)
     }
-  }, [loadMore])
+  }, [fetchNextPage, isFetchingNextPage])
 
   return (
     <div className="relative flex h-full flex-col bg-white">
@@ -92,7 +78,7 @@ const Community = (): JSX.Element => {
             ref={loader}
             className="text-c1-regular flex items-center justify-center bg-white py-3 text-gray-500"
           >
-            {posts.length < dummyCommunityPosts.length ? 'Loading...' : '마지막 게시글입니다.'}
+            {hasNextPage ? <BabyIcon className="size-8 fill-green-200" /> : '마지막 게시글입니다.'}
           </div>
         </div>
       </div>
